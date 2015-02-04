b3f3ef123ed698ebc38c3a67fc693d5e5858b052
Diogo <diogo.mvieira@labsynapse.com>
2015-02-04 13:48:08 -0200

    resolved #6 with a beautiful interface that list hostels by city using api

--- interface/app/static/js/script.js
+++ interface/app/static/js/script.js
@@ -1,11 +1,26 @@
 // Default file for javascript code in search interface
 
 function gen_new_suggestion(text){
+    // Generate html for autocomplete suggestions
     code = "<li class='suggest'>"+text+"</li>";
     return code;
 }
 
+function gen_new_result(object){
+    // Generate html for autocomplete suggestions
+    code = "<div class='col-xs-12'>" +
+           "<hr>" +
+           "<h4>"+object.name+"</h4>" +
+           "<p>"+object.location+"</p>" +
+           "<small>"+object.phone+"</small>" +
+           "</div>";
+    return code;
+}
+
 $(function() {
+
+    // Monitoring search-input for each keyup
+    // It's a simple autocomplete made because I had errors with mongodb _id
     $("#search-input").keyup(function(){
         $.ajax({
 
@@ -16,16 +31,21 @@ $(function() {
                 var objects = JSON.parse(data);
                 $('#search-suggestions').html('');
                 for (i = 0; i < objects.length; i++){
+                    // add suggestions to html
                     $('#search-suggestions').html($('#search-suggestions').html()+gen_new_suggestion(objects[i]._id));
                 }
                 $('#search-suggestions').html($('#search-suggestions').html()+'</ul>');
             });
     });
+
+    // Monitoring each suggestion for click event.
+    // When clicked, suggestion goes to input and force html clean
     $("#search-suggestions").on('click', '.suggest', function(){
         $("#search-input").val($(this).html());
         $("#search-suggestions").html('');
     })
 
+    // Intercepts submit call and load hotels by ajax
     $("#search-form form").submit(function(event) {
         event.preventDefault();
 
@@ -36,9 +56,9 @@ $(function() {
     })
   .done(function( data ) {
       var objects = JSON.parse(data);
-      $('#search-suggestions').html('');
+      $('#search-results').html('<h1>Results for '+$("#search-input").val()+'</h1>');
       for (i = 0; i < objects.length; i++){
-          console.log(objects[i]);
+          $('#search-results').html($('#search-results').html()+gen_new_result(objects[i]));
       }
     })
 
